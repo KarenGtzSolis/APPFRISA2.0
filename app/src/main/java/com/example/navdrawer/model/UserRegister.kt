74b2be64ad9354003a74bfe40aa30ec1eae4d301
nachomi24 <66391621+nachomi24@users.noreply.github.com>
2023-10-11 20:53:34 -0600

    Updated UserRegister dataclas

--- app/src/main/java/com/example/navdrawer/model/UserRegister.kt
+++ app/src/main/java/com/example/navdrawer/model/UserRegister.kt
@@ -1,11 +1,11 @@
 package com.example.navdrawer.model
 
 data class UserRegister(
-    val name: String, // Puede ser nulo
-    val last_name: String, // Puede ser nulo
-    val phone: Int, // Puede ser nulo
-    val email: String?, // Puede ser nulo
-    val password: String, // Puede ser nulo
-    val tags: List<String>, // Puede ser una lista vacía
-    val favorites: List<String> // Puede ser una lista vacía
+    val name: String ="", // Puede ser nulo
+    val last_name: String="", // Puede ser nulo
+    val phone: Int=0, // Puede ser nulo
+    //val email: String?="", // Puede ser nulo
+    val password: String?="", // Puede ser nulo
+    //val tags: List<String> = emptyList(), // Puede ser una lista vacía
+    //val favorites: List<String> = emptyList() // Puede ser una lista vacía
 )
