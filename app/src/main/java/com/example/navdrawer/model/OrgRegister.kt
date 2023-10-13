4f8f1f3c6cb6997ee54e69856374b6198b6170cf
nachomi24 <66391621+nachomi24@users.noreply.github.com>
2023-10-13 14:51:48 -0600

    Update OrgRegister.kt
    Registro de organizaciones listo

--- app/src/main/java/com/example/navdrawer/model/OrgRegister.kt
+++ app/src/main/java/com/example/navdrawer/model/OrgRegister.kt
@@ -1,19 +1,21 @@
+package com.example.navdrawer.model
+
 data class OrgRegister(
-    val city: String,
-    val description: String,
-    val email: String,
-    val image: String,
-    val linkFacebook: String,
-    val linkInstagram: String,
-    val linkOther: String,
-    val linkTwitter: String,
-    val linkWeb: String,
-    val password: String,
-    val phone: String,
-    val posts: String,
-    val schedule: String,
-    val street: String,
-    val suburb: String,
-
+    val name: String?="",
+    val phone: Int=0,
+    val email: String?="",
+    val street: String?="",
+    val suburb: String?="",
+    val city: String?="",
+    val state: String?="",
+    val schedule: String?="",
+    val linkWeb: String?="",
+    val linkFacebook: String?="",
+    val linkInstagram: String?="",
+    val linkTwitter: String?="",
+    val linkOther: String?="",
+    val description: String?="",
+    val image: String?="",
+    val tags: List<String> = emptyList(),
+    val password: String?=""
 )
-
