0c03508e81b6a7a1cc0a805c8f1459f27bb572bc
nachomi24 <66391621+nachomi24@users.noreply.github.com>
2023-10-13 14:54:48 -0600

    Update OrgService.kt
    Serivicio de org listo

--- app/src/main/java/com/example/navdrawer/service/OrgService.kt
+++ app/src/main/java/com/example/navdrawer/service/OrgService.kt
@@ -1,42 +1,29 @@
 package com.example.navdrawer.service
 
-
-
-import OrgRegister
-import com.example.navdrawer.model.OrganizationResponse
-
-
+import com.example.navdrawer.model.OrgLogin
+import com.example.navdrawer.model.OrgLoginResponse
+import com.example.navdrawer.model.OrgRegister
+import com.example.navdrawer.model.OrgRegisterResponse
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface OrgService {
 
-    //@POST("organizations/add") // Incluye la ruta específica del endpoint aquí
-    //suspend fun addOrg(@Header("Authorization") token: String, @Body org: OrgRegister): OrgRegisterResponse
-
-
-    //@GET("organizations/all")
-    //suspend fun getAllOrgs(): List<OrgRegister>
-
-
-    //@GET("organizations/all")
-    //suspend fun getAllOrgs(): List<OrgRegister>
-
-
-
     companion object {
-
-        val instance: OrgService = Retrofit.Builder().baseUrl("https://androidapi-8n8l-dev.fl0.io/api/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(OrgService::class.java)
+        val instance: OrgService =
+            Retrofit.Builder().baseUrl("https://androidapi-8n8l-dev.fl0.io/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(OrgService::class.java)
     }
 
+    @POST("organization/register") // Incluye la ruta específica del endpoint aquí
+    suspend fun insertOrg(@Body org: OrgRegister): OrgRegisterResponse
 
-    @GET("organizations/all")
-    suspend fun getAllOrgs(): OrganizationResponse
+    @POST("organization/login")
+    suspend fun loginOrg(@Body user: OrgLogin): OrgLoginResponse
+
 }
