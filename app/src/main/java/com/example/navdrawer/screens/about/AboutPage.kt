6eb458e84a160a0a93fcbd74b1c2e694a67761a8
Carolina Lara <carolara140@gmail.com>
2023-10-13 14:43:00 -0600

    about page carga datos

--- app/src/main/java/com/example/navdrawer/screens/about/AboutPage.kt
+++ app/src/main/java/com/example/navdrawer/screens/about/AboutPage.kt
@@ -1,73 +1,81 @@
 package com.example.navdrawer.screens.about
-
-import android.provider.Contacts
 import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Transparent
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavHostController
-import coil.compose.rememberImagePainter
-import com.example.navdrawer.R
+import coil.compose.AsyncImage
+import com.example.navdrawer.UserViewModel.OrganizationViewModel
+import com.example.navdrawer.model.OrganizationResponse
 import com.example.navdrawer.screens.home.OrgRow
+import com.example.navdrawer.service.OrgService
 
 
 @Composable
-fun AboutPage(orgname: String = "Sin Nombre", navController: NavHostController) {
-
+fun AboutPage(orgname: String = "", navController: NavHostController) {
     Column {
         Text(text = "Aquí se muestra la info de una organización")
     }
-    Box(contentAlignment= Alignment.Center,
-        modifier= Modifier.fillMaxSize()){
-        Text(text="$orgname",
-            style =
-            TextStyle(
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val orgViewModel = OrganizationViewModel(OrgService.instance)
+
+        val Organizations = remember {
+            mutableStateOf(OrganizationResponse())
+        }
+
+        // Obtén los detalles de la organización con el nombre especificado
+        val organization = Organizations.value.find { it.name == orgname }
+
+        organization?.let {
+            // Organizacion encontrada con el nombre especificado
+            Text(text = organization.name, style = TextStyle(
                 color = Black,
-                textAlign = androidx.compose.ui.text.style.TextAlign.Center,
-                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
-
-
-
-
-
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
             ))
+            Text(text = organization.description, style = TextStyle(
+                color = Black,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
+            ))
+            AsyncImage(
+                model = organization.image,
+                contentDescription = null,
+            )
+        }
+
+        LaunchedEffect(key1 = orgViewModel.getOrgResult) {
+            orgViewModel.getUserFavoriteOrganization()
+            orgViewModel.getOrgResult.collect { result ->
+                if (result != null) {
+                    val organizationsResponse = OrganizationResponse()
+                    organizationsResponse.addAll(result)
+                    Organizations.value = organizationsResponse
+                }
+            }
+        }
     }
-
-
-
-
 }
 
 
 
-@Composable
-fun detalles(){
 
 
-
-
-
-}
 
 
 
