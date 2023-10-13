4cfa41b5aefdf119f347931a92e72affd1ee1b50
Carolina Lara <carolara140@gmail.com>
2023-10-12 18:03:53 -0600

    Org data class checked

---
+++ app/src/main/java/com/example/navdrawer/OrganizationViewModel/OrganizationViewModel.kt
@@ -0,0 +1,9 @@
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+import com.example.navdrawer.service.OrgService // Asegúrate de importar OrgService
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+
--- app/src/main/java/com/example/navdrawer/service/OrgService.kt
+++ app/src/main/java/com/example/navdrawer/service/OrgService.kt
@@ -5,6 +5,7 @@ import com.example.navdrawer.model.OrgRegisterResponse
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -13,9 +14,12 @@ interface OrgService {
     @POST("organizations/add") // Incluye la ruta específica del endpoint aquí
     suspend fun addOrg(@Header("Authorization") token: String, @Body org: OrgRegister): OrgRegisterResponse
 
+    @GET("organizations/all")
+    suspend fun getAllOrgs(): List<OrgRegister>
+
     companion object {
         // Especifica la URL base sin incluir la ruta específica del endpoint
-        private const val BASE_URL = "https://apifrisa-py6x-dev.fl0.io/"
+        private const val BASE_URL = "https://androidapi-8n8l-dev.fl0.io/api/"
 
         fun create(): OrgService {
             val retrofit = Retrofit.Builder()
