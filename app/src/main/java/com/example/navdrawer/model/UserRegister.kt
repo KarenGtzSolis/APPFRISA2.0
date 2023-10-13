0ce8f6e04a26d9c23fccd118794447a769cdeb07
nachomi24 <66391621+nachomi24@users.noreply.github.com>
2023-10-13 15:12:32 -0600

    Update UserRegister.kt
    added description for register

--- app/src/main/java/com/example/navdrawer/model/UserRegister.kt
+++ app/src/main/java/com/example/navdrawer/model/UserRegister.kt
@@ -2,10 +2,11 @@ package com.example.navdrawer.model
 
 data class UserRegister(
     val name: String ="", // Puede ser nulo
-    val last_name: String="", // Puede ser nulo
+    val lastname: String="", // Puede ser nulo
     val phone: Int=0, // Puede ser nulo
     //val email: String?="", // Puede ser nulo
     val password: String?="", // Puede ser nulo
+    val description: String?=""
     //val tags: List<String> = emptyList(), // Puede ser una lista vacía
     //val favorites: List<String> = emptyList() // Puede ser una lista vacía
 )
